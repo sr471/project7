61e4c6c4ebeb62a793c0726b0198b900298457f8
Shanthi robinson <shanthi.robinson100@gmail.com>
2016-12-12 12:15:18 -0800

    Start up

--- project7.js
+++ project7.js
@@ -10,13 +10,48 @@ function transformPoint(event) {
   return mousept
 }
 
-// Step 2: drawSquare and drawCircle functions
+
 function drawSquare(x, y, size, color) {
-  // square drawing code here
+var newsquare = document.createElementNS(namespace, "rect")
+newsquare.setAttribute("x", x)
+newsquare.setAttribute("y", y)
+newsquare.setAttribute("width", size)
+newsquare.setAttribute("height", size)
+newsquare.setAttribute("fill", color)
+screen.appendChild(newsquare)
 }
 
+function drawCircle(x, y, size, color) {
+  var newcircle = document.createElementNS(namespace, "circle")
+  newcircle.setAttribute("cx", x)
+  newcircle.setAttribute("cy", y)
+  newcircle.setAttribute("fill", color)
+  newcircle.setAttribute("r", size)
+  screen.appendChild(newcircle)
+}
+
+var drawing = false;
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
+  drawing = true;
+})
+
+document.addEventListener("mouseup", function(e) {
+drawing = false;
   // what do you want to do when the user presses down
   // on the mouse button?
+})
+
+document.addEventListener("mousemove", function(e) {
+var color = document.getElementById("colorSelect").value
+var shape = document.getElementById("shapeSelect").value
+var size = document.getElementById("sizeSelect").value
+
+  if (drawing){
+    var pt = transformPoint(e, screen)
+
+    if(shape == "circle")
+  drawCircle(pt.x, pt.y, 50, "purple")
+  }
+  
 })
